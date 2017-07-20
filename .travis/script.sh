1d3cbde652b3fe5c7d46699b04abc27ce6920e12
Jack <me@jarrekk.com>
2017-07-20 10:26:55 +0800

    edit script

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,7 +3,8 @@
 # check picture size
 ERROR='false'
 for i in $(ls thumbnails);do
-    SIZE=$(identify ${i}|cut -d' ' -f3)
+    SIZE=$(identify thumbnails/${i}|cut -d' ' -f3)
+    if [ $? != 0 ];then exit 1;fi
     if [ ${SIZE} != '250x200' ];then
         echo "File ${i} size is not correct!"
         ERROR='true'
