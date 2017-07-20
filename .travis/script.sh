cfa93ef234a48f8bab7d3c02cdd0802b6cf335d0
Jack <me@jarrekk.com>
2017-07-20 10:56:08 +0800

    edit script

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # check picture size
 ERROR='false'
 for i in $(ls thumbnails);do
-    SIZE=$(identify thumbnails/${i}|cut -d' ' -f3)
-    if [ $? != 0 ];then exit 1;fi
+    INFO=$(identify thumbnails/${i})
+    SIZE=$(echo ${INFO}|cut -d' ' -f3)
     if [ ${SIZE} != '250x200' ];then
-        echo "File ${i} size is not correct!"
-        ERROR='true'
+        echo "${YELLOW}File ${i} size is not correct! It's size is ${SIZE}.${NC}"
+        convert -resize 250x200! -quality 100 thumbnails/${i} thumbnails/${i}
+        if [ $? != 0 ];then
+            echo "${YELLOW}Picture ${i} convert failed!${NC}"
+            ERROR='true'
+        else
+            echo "Picture ${i} convert finished."
+        fi
     fi
 done
 if [ ${ERROR} == 'true' ];then
     exit 1
+else
+    echo "${YELLOW}All pictures are correct now!${NC}"
 fi
