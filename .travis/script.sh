997c69eb004f4a78582451a73197b8c0052e0107
Jack <me@jarrekk.com>
2017-07-20 12:15:53 +0800

    edit script

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,18 +9,18 @@ for i in $(ls thumbnails);do
     INFO=$(identify thumbnails/${i})
     SIZE=$(echo ${INFO}|cut -d' ' -f3)
     if [ ${SIZE} != '250x200' ];then
-        echo "${YELLOW}File ${i} size is not correct! It's size is ${SIZE}.${NC}"
+        echo -e "${YELLOW}File ${i} size is not correct! It's size is ${SIZE}.${NC}"
         convert -resize 250x200! -quality 100 thumbnails/${i} thumbnails/${i}
         if [ $? != 0 ];then
-            echo "${YELLOW}Picture ${i} convert failed!${NC}"
+            echo -e "${YELLOW}Picture ${i} convert failed!${NC}"
             ERROR='true'
         else
-            echo "Picture ${i} convert finished."
+            echo -e "Picture ${i} convert finished."
         fi
     fi
 done
 if [ ${ERROR} == 'true' ];then
     exit 1
 else
-    echo "${YELLOW}All pictures are correct now!${NC}"
+    echo -e "${YELLOW}All pictures are correct now!${NC}"
 fi
